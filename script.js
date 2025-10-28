9a62d3042d21373b7122c9f680f3aa2050b457ec
MinecraftMissionoperation <S040399@students.lmsd.org>
2025-10-28 17:11:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let canvas, ctx;
 let basket, stars, score, lives, keys;
 let gameRunning = false;
-let starInterval;
+let starInterval, scoreInterval, levelInterval;
+let level = 1;
+let starSpeed = 3;
 let catchSound, missSound;
 
 function setupGame() {
@@ -12,10 +14,14 @@ function setupGame() {
   stars = [];
   score = 0;
   lives = 5;
+  level = 1;
+  starSpeed = 3;
   keys = {};
 
   document.getElementById("score").innerText = "Score: 0";
   document.getElementById("lives").innerText = "Lives: 5";
+  document.getElementById("level").innerText = "Level: 1";
+  document.getElementById("gameOver").style.display = "none";
 
   catchSound = new Audio("assets/catch.mp3");
   missSound = new Audio("assets/miss.mp3");
@@ -23,15 +29,21 @@ function setupGame() {
 
 function drawBasket() {
   ctx.fillStyle = "#FFD700";
+  ctx.shadowColor = "#FFD700";
+  ctx.shadowBlur = 20;
   ctx.fillRect(basket.x, basket.y, basket.width, basket.height);
+  ctx.shadowBlur = 0;
 }
 
 function drawStar(star) {
   ctx.beginPath();
   ctx.arc(star.x, star.y, star.radius, 0, Math.PI * 2);
   ctx.fillStyle = "#00FFFF";
+  ctx.shadowColor = "#00FFFF";
+  ctx.shadowBlur = 15;
   ctx.fill();
   ctx.closePath();
+  ctx.shadowBlur = 0;
 }
 
 function spawnStar() {
@@ -41,14 +53,14 @@ function spawnStar() {
 
 function updateStars() {
   for (let i = stars.length - 1; i >= 0; i--) {
-    stars[i].y += 3 + score * 0.1;
+    stars[i].y += starSpeed;
     if (
       stars[i].y + stars[i].radius > basket.y &&
       stars[i].x > basket.x &&
       stars[i].x < basket.x + basket.width
     ) {
       stars.splice(i, 1);
-      score++;
+      score += 5;
       catchSound.play();
       document.getElementById("score").innerText = "Score: " + score;
     } else if (stars[i].y > canvas.height) {
@@ -62,8 +74,8 @@ function updateStars() {
 }
 
 function moveBasket() {
-  if (keys["ArrowLeft"] && basket.x > 0) basket.x -= 5;
-  if (keys["ArrowRight"] && basket.x < canvas.width - basket.width) basket.x += 5;
+  if (keys["ArrowLeft"] && basket.x > 0) basket.x -= 6;
+  if (keys["ArrowRight"] && basket.x < canvas.width - basket.width) basket.x += 6;
 }
 
 function gameLoop() {
@@ -79,14 +91,35 @@ function gameLoop() {
 function endGame() {
   gameRunning = false;
   clearInterval(starInterval);
-  alert("Game Over! Final Score: " + score);
+  clearInterval(scoreInterval);
+  clearInterval(levelInterval);
+  document.getElementById("finalScore").innerText = "Final Score: " + score;
+  document.getElementById("gameOver").style.display = "block";
+}
+
+function restartGame() {
+  setupGame();
+  startGame();
+}
+
+function startGame() {
+  gameRunning = true;
+  starInterval = setInterval(spawnStar, 1000);
+  scoreInterval = setInterval(() => {
+    score++;
+    document.getElementById("score").innerText = "Score: " + score;
+  }, 1000);
+  levelInterval = setInterval(() => {
+    level++;
+    starSpeed += 1;
+    document.getElementById("level").innerText = "Level: " + level;
+  }, 15000);
+  gameLoop();
 }
 
 document.getElementById("startBtn").onclick = function () {
   setupGame();
-  gameRunning = true;
-  starInterval = setInterval(spawnStar, 1000);
-  gameLoop();
+  startGame();
 };
 
 window.addEventListener("keydown", e => keys[e.key] = true);
